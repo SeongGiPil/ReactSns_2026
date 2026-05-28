2d46346e196b655c03348748df9e521e55da2a89
SeongGiPil <96853746+SeongGiPil@users.noreply.github.com>
2026-05-28 10:13:09 +0900

    2026_0528

--- front/src/components/ContextEx1.js
+++ front/src/components/ContextEx1.js
@@ -1,14 +1,20 @@
+import { useState } from "react"
+
 function content1(){
     return<>
         <h5>content1!!!</h5>
+        <div>이름,나이출력</div>
     </>
 }
 
 function Main(){
     return<>
         <h4>메인!</h4>
+        <content1></content1>
+        <content2></content2>
     </>
 };
+
 function Lside(){return<></>};
 function Rside(){return<></>};
 function Body(){
@@ -18,11 +24,12 @@ function Body(){
 }
 
 function ContextEx1(){
+    let [name,setName]=useState("");
+    let[age,setAge]=useState(30);
+    
     return<>
         <Body></Body>
-        <Lside></Lside>
-        <Rside></Rside>
-        <Main></Main>
+    
     </>
 }
 
